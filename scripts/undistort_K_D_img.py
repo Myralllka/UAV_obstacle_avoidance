cc121ab71adce30c4e77d4de159a1c44b6bfbbcd
Myralllka <Olli25010@gmail.com>
2021-12-03 11:48:06 +0100

    cameras calibrated, updated scripts

--- scripts/undistort_K_D_img.py
+++ scripts/undistort_K_D_img.py
@@ -5,15 +5,15 @@ import sys
 import cv2
 
 # You should replace these 3 lines with the output in calibration step
-DIM=(1600, 1200)
+DIM = (1600, 1200)
+
 
 def undistort(m_K, m_D, img_path):
     img = cv2.imread(img_path)
-    h,w = img.shape[:2]
-    map1, map2 = cv2.fisheye.initUndistortRectifyMap(m_K, m_D, np.eye(3), m_K, DIM, cv2.CV_16SC2)
+    h, w = img.shape[:2]
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(m_K, m_D, np.eye(3), m_K, (h,w), cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
     cv2.imwrite(img_path.replace("jpeg", "undistored.jpeg"), undistorted_img)
-
 
 
 if __name__ == '__main__':
@@ -21,4 +21,3 @@ if __name__ == '__main__':
     D = np.loadtxt(sys.argv[2])
     for p in sys.argv[3:]:
         undistort(K, D, p)
-
