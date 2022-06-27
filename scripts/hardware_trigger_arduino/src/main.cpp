f09e9261866b2f55a6a4fc011f2687ecefbe35d2
Myralllka <Olli25010@gmail.com>
2022-06-27 20:04:06 +0200

    working on the frequancy improvement

--- scripts/hardware_trigger_arduino/src/main.cpp
+++ scripts/hardware_trigger_arduino/src/main.cpp
@@ -6,8 +6,10 @@ void setup() {
 
 void loop() {
     // 10 ms with 8e10^6 f_cpu for ArduinoNano gives 20ms -> 50fps
+    // 12 gives 24ms -> 41fps
+    int a = 12;
     digitalWrite(PIND2, HIGH);
-    delay(10);
+    delay(a);
     digitalWrite(PIND2, LOW);
-    delay(10);
+    delay(a);
 }
