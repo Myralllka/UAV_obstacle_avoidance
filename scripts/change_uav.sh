adce55cba52fee1e212d6d25cfb33aeaf7474d0c
Myralllka <Olli25010@gmail.com>
2022-07-20 20:31:31 +0200

    working on drones random number in rviz

---
+++ scripts/change_uav.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get path to script
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $SCRIPT_PATH
+if [ -x "$(whereis nvim | awk '{print $2}')" ]; then
+ VIM_BIN="$(whereis nvim | awk '{print $2}')"
+ HEADLESS="--headless"
+elif [ -x "$(whereis vim | awk '{print $2}')" ]; then
+ VIM_BIN="$(whereis vim | awk '{print $2}')"
+ HEADLESS=""
+fi
+
+echo "$VIM_BIN -u '$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc' $HEADLESS -Ens -c '%s/uav[0-9]\+/$1/g' -c 'wqa' -- '$SCRIPT_PATH/../rviz/device.rviz'"
+$VIM_BIN -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -Ens -c "%s/uav[0-9]\+/$1/g" -c "wqa" -- "$SCRIPT_PATH/../rviz/simulation.rviz"
